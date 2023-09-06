f345c749c26ded5be899417c3570023444794159
Sumanth Chinthagunta <xmlking@gmail.com>
2023-09-06 07:49:51 -0700

    fix: add offline [skip ci]

---
+++ apps/offline/TODO1.md
@@ -0,0 +1,9 @@
+
+https://www.youtube.com/playlist?list=PLIyDDWd5rhabADfiS00oTcEdVAmQJzdVx
+https://github.com/jpwhite3/northwind-SQLite3
+
+https://github.com/phartenfeller/sveltekit-sqlite-offline-app/tree/part1
+https://github.com/phartenfeller/sveltekit-sqlite-offline-app/tree/part2
+https://github.com/phartenfeller/sveltekit-sqlite-offline-app/tree/part3
+https://github.com/phartenfeller/sveltekit-sqlite-offline-app/tree/part4
+ 
---
+++ apps/offline/tailwind.config.ts
@@ -0,0 +1,67 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { sumo } from './src/sumo'
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		forms,
+		typography,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+					{
+						name: 'wintry',
+						enhancements: true,
+					},
+					{
+						name: 'modern',
+						enhancements: true,
+					},
+					{
+						name: 'hamlindigo',
+						enhancements: true,
+					},
+					{
+						name: 'rocket',
+						enhancements: true,
+					},
+					{
+						name: 'sahara',
+						enhancements: true,
+					},
+					{
+						name: 'gold-nouveau',
+						enhancements: true,
+					},
+					{
+						name: 'vintage',
+						enhancements: true,
+					},
+					{
+						name: 'seafoam',
+						enhancements: true,
+					},
+					{
+						name: 'crimson',
+						enhancements: true,
+					},
+				],
+				custom: [
+					sumo,
+				],
+			},
+		}),
+	],
+} satisfies Config;
