232f0a2f20c4839cb828d7eab3310fdcaa53413d
Sumanth Chinthagunta <xmlking@gmail.com>
2023-09-10 15:44:10 -0700

    fix: testing deploy

--- apps/playground/svelte.config.js
+++ apps/playground/svelte.config.js
@@ -1,4 +1,6 @@
 import adapter from '@sveltejs/adapter-node';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterAuto from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,7 +17,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: process.env.VERCEL ? adapterAuto() : adapterNode(),
 		prerender: { crawl: false }, // FIXME: remove after all links are corrected.
 		output: {
 			preloadStrategy: 'preload-mjs'
