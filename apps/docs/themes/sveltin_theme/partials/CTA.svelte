eb2b66f70c7a7c4836bdaa82811c39f3908fd4d6
Sumanth Chinthagunta <xmlking@gmail.com>
2023-10-02 01:31:49 -0700

    fix(docs): remove themer

--- apps/docs/themes/sveltin_theme/partials/CTA.svelte
+++ apps/docs/themes/sveltin_theme/partials/CTA.svelte
@@ -1,40 +1,45 @@
 <script lang="ts">
-	import { Link } from '@sveltinio/essentials';
+
 </script>
 
 <section class="mx-auto px-6 py-44 max-w-7xl">
-	<h1
-		class="pb-4 mx-auto text-center text-skin-base text-3xl font-bold tracking-tight sm:text-4xl lg:text-5xl lg:leading-tight"
-	>
-		Your Sveltin project is <span
-			class="block italic font-normal text-skin-accent capitalize"
-			>up & running!</span
-		>
-	</h1>
-
-	<div class="relative inline-flex justify-center w-full mx-auto mt-4">
-		<Link
-			id="quickStartLink"
-			external
-			icon={false}
-			href="https://docs.sveltin.io/quick-start"
-			class="relative inline-flex items-center p-2 text-white text-sm font-medium bg-river border-2 border-river rounded-l-lg hover:bg-haiti focus:outline-none lg:px-8"
-			>Quick Start</Link
-		>
-
-		<Link
-			id="documentationLink"
-			external
-			icon={false}
-			href="https://docs.sveltin.io"
-			class="relative inline-flex items-center p-2 -ml-px text-sm font-medium border-2 border-river rounded-r-lg focus:outline-none lg:px-8"
-			>Read the Docs</Link
-		>
-	</div>
-
-	<div class="relative inline-flex justify-center w-full mx-auto mt-4">
-		<Link href="/posts" icon={false} class="relative inline-flex items-center p-2 text-white text-sm font-medium bg-river border-2 border-river rounded-l-lg hover:bg-haiti focus:outline-none lg:px-8" >Blog Posts</Link>
-		<Link href="/contact" icon={false} class="relative inline-flex items-center p-2 -ml-px text-sm font-medium border-2 border-river focus:outline-none lg:px-8">Contact</Link>
-		<Link href="/about" icon={false} class="relative inline-flex items-center p-2 -ml-px text-sm font-medium border-2 border-river rounded-r-lg focus:outline-none lg:px-8">About</Link>
-	</div>
+		<div class="text-center py-8 lg:py-12">
+			<h1 class="whitespace-nowrap text-5xl md:text-6xl lg:text-7xl font-bold">Spectacular</h1>
+			<h1>
+				<span
+					class="bg-gradient-to-br from-primary-500 to-secondary-500 bg-clip-text text-transparent box-decoration-clone
+					text-5xl md:text-6xl lg:text-7xl font-bold"
+				>
+					Anywhere
+				</span>
+			</h1>
+			<h1>
+				<span
+					class="bg-gradient-to-br from-primary-500 via-secondary-500 to-tertiary-500 bg-clip-text text-transparent box-decoration-clone
+					text-5xl md:text-6xl lg:text-7xl font-bold"
+				>
+					Any Device
+				</span>
+			</h1>
+		</div>
+		<div class="flex flex-wrap justify-center gap-2">
+			<a class="btn variant-filled-primary btn-base" href="/posts">Docs</a>
+			<a class="btn variant-filled-secondary btn-base" href="/posts">Getting Started</a>
+			<a class="btn variant-filled-tertiary btn-base" href="https://github.com/xmlking/spectacular" target="_blank">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					x="0px"
+					y="0px"
+					viewBox="0 0 512 512"
+					class="svg-icon inline-block outline-none fill-current w-5 h-5 mr-2"
+					focusable="false"
+					data-testid="svg-icon"
+				>
+					<path
+						d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+					/>
+				</svg>
+				Github
+			</a>
+		</div>
 </section>
