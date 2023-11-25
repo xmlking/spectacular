b90e25b9dc1ca6aee9b34df29f5bcd7119dc5475
Sumanth Chinthagunta <xmlking@gmail.com>
2023-11-25 13:51:10 -0800

    fix: init script checking if running in CI

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,13 +4,13 @@ cd "$(dirname $0)/.."
 # cp -n .env.example .env && echo 'Generated: .env'
 # cp -n apps/console/.env.example apps/console/.env && echo 'Generated: apps/console/.env'
 FILE=.env
-if [[ -f "$FILE" || "$CI" == "true" ]]; then
+if [[ -f "$FILE" || -n "$CI" ]]; then
     echo "'$FILE' exists."
 else
 	cp -n .env.example .env && echo 'Generated: .env'
 fi
 FILE=apps/web/.env
-if [[ -f "$FILE" || "$CI" == "true" ]]; then
+if [[ -f "$FILE" || -n "$CI" ]]; then
     echo "'$FILE' exists."
 else
 	cp -n apps/console/.env.example apps/console/.env && echo 'Generated: apps/console/.env'
