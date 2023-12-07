7fac569c6710a968bcdadc5abd491461586bef8b
Sumanth Chinthagunta <xmlking@gmail.com>
2023-12-06 16:04:27 -0800

    fix: add vercel for astro

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import svelte from '@astrojs/svelte';
+// import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), svelte()]
+	integrations: [tailwind(), svelte()],
+	output: 'static',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true
+		},
+		speedInsights: {
+			enabled: true
+		}
+	})
 });
