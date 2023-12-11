c286a59c7af5d1f91ff0d9ab90adc56d5c70bc62
Sumanth Chinthagunta <xmlking@gmail.com>
2023-12-11 14:53:05 -0800

    fix(console): add console2

---
+++ apps/console2/tailwind.config.ts
@@ -0,0 +1,39 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { sumo } from './src/sumo'
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		forms,
+		typography,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+					{
+						name: 'wintry',
+						enhancements: true,
+					},
+					{
+						name: 'modern',
+						enhancements: true,
+					},
+				],
+				custom: [
+					sumo,
+				],
+			},
+		}),
+	],
+} satisfies Config;
