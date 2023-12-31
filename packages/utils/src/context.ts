4bd34ba66f8401dc596b3be0b30e5c23ea119c11
Sumanth Chinthagunta <xmlking@gmail.com>
2023-12-31 01:15:59 -0800

    chore: deps updated

--- packages/utils/src/context.ts
+++ packages/utils/src/context.ts
@@ -4,29 +4,29 @@
  * Ref: https://github.com/KamenKolev/svelte-typed-context
  */
 import {
-  getContext as svelteGetContext,
-  setContext as svelteSetContext,
-  hasContext as svelteHasContext,
-} from "svelte"
+	getContext as svelteGetContext,
+	setContext as svelteSetContext,
+	hasContext as svelteHasContext
+} from 'svelte';
 
 declare const isChecked: unique symbol;
 
 /**
  * Provided as key to getContext and setContext in order to enable strict typing
  */
-export interface InjectionKey<T = unknown> { }
+export interface InjectionKey<T = unknown> {}
 
 export interface CheckedInjectionKey<T> extends InjectionKey<T> {
-  [isChecked]?: never;
+	[isChecked]?: never;
 }
 
 type getContext = {
-  <T>(key: CheckedInjectionKey<T>): T
-  <T>(key: InjectionKey<T>): undefined | T
-}
-type setContext = <T>(key: InjectionKey<T>, context: T) => void
-type hasContext = <T>(key: InjectionKey<T>) => key is CheckedInjectionKey<T>
+	<T>(key: CheckedInjectionKey<T>): T;
+	<T>(key: InjectionKey<T>): undefined | T;
+};
+type setContext = <T>(key: InjectionKey<T>, context: T) => void;
+type hasContext = <T>(key: InjectionKey<T>) => key is CheckedInjectionKey<T>;
 
-export const getContext = svelteGetContext as getContext
-export const setContext = svelteSetContext as setContext
-export const hasContext = svelteHasContext as hasContext
+export const getContext = svelteGetContext as getContext;
+export const setContext = svelteSetContext as setContext;
+export const hasContext = svelteHasContext as hasContext;
