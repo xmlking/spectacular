c49d236eec825fbda7d4030aa64ef835413d0235
Sumanth Chinthagunta <xmlking@gmail.com>
2024-06-09 16:09:41 -0700

    fix: Tailwind CSS Typography class: prose applied for legal pages

--- apps/console/src/routes/(legal)/+layout@.svelte
+++ apps/console/src/routes/(legal)/+layout@.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-import { User2 } from 'lucide-svelte';
+import { UsersRound } from 'lucide-svelte';
 </script>
 
 <div class="mx-auto flex max-w-2xl flex-col justify-center px-6 py-12 lg:px-8">
   <div class="card p-6 pb-8 pt-8">
-    <div>
+    <div class="prose xl:prose-xl lg:prose-lg md:prose-base sm:prose-sm dark:prose-invert">
       <div class="mb-4 flex flex-row items-center justify-center">
         <div>
-          <User2 size={42} />
+          <UsersRound size={42} />
         </div>
       </div>
       <slot />
