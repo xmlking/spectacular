33a2b8b5e3735d53f9cb779ee8d86b8d9ad74732
Sumanth Chinthagunta <sumanth@chinthagunta.com>
2024-07-15 22:39:40 -0700

    fix(nhost): auth tests

--- apps/console/src/routes/(app)/profile/components/connect-socials.svelte
+++ apps/console/src/routes/(app)/profile/components/connect-socials.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 import { type AuthProvidersFragment, PendingValue, fragment, graphql } from '$houdini';
-
+import { getNhostClient } from '$lib/stores/nhost';
+import type { NhostClient, Provider } from '@nhost/nhost-js';
+import { Icon } from '@spectacular/skeleton/components/icons';
+import { Github } from 'lucide-svelte';
+const nhost = getNhostClient();
 export let user: AuthProvidersFragment;
 $: data = fragment(
   user,
@@ -16,47 +20,50 @@ $: data = fragment(
   `),
 );
 $: ({ providers } = $data);
+
+let Ids: string[] = [];
+$: Ids = providers?.map((provider) => provider.providerId);
+$: isGithub = Ids?.includes('github');
+$: isAzure = Ids?.includes('azuread');
+$: isGoogle = Ids?.includes('google');
+async function login(nhost: NhostClient, provider: Provider) {
+  const { providerUrl } = await nhost.auth.connectProvider({
+    provider,
+  });
+}
+function click(event: MouseEvent, provider: Provider) {
+  login(nhost, provider);
+}
 </script>
 
-<div class="card p-4">
-  <div class="table-container">
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>providerId</th>
-          <th>providerUserId</th>
-          <th>updatedAt</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each providers as provider, i}
-          {#if provider === PendingValue}
-            <tr class="animate-pulse">
-              <td><div class="placeholder" /></td>
-              <td><div class="placeholder" /></td>
-              <td><div class="placeholder" /></td>
-              <td><div class="placeholder" /></td>
-            </tr>
-          {:else}
-            <tr>
-              <td>{provider.id}</td>
-              <td>{provider.providerId}</td>
-              <td>{provider.providerUserId}</td>
-              <td>{provider.updatedAt}</td>
-            </tr>
-          {/if}
-        {:else}
-          <tr>
-            <td colspan="4"
-              ><div class="text-center text-gray-500">
-                No org roles found.
-              </div></td
-            >
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  </div>
+<div class="card p-4 flex">
+  {#if !isGithub}
+      <button
+        type="button"
+        class="variant-filled-primary btn ml-0"
+        on:click={(event)=>click(event,'github')}
+      >
+      <Github/>&nbsp; Connect with Github</button>
+    {:else}
+      <button type="button" class="variant-ghost-primary btn ml-0"><Github/>&nbsp; Github Connnected</button>
+  {/if}
+  {#if !isAzure}
+      <button
+        class="variant-filled-primary btn ml-auto"
+        on:click={(event)=>click(event,'azuread')}
+      ><Icon name="microsoft" />&nbsp; Connect with Microsoft</button
+    >
+    {:else}
+       <button type="button" class="variant-ghost-primary btn ml-auto"><Icon name="microsoft" />&nbsp; Microsoft Connnected</button>
+  {/if}
+  {#if !isGoogle}
+      <button
+        type="button"
+        class="variant-filled-primary btn ml-auto"
+        on:click={(event)=>click(event,'google')}
+      ><Icon name="google" />&nbsp; Connect with Google</button
+    >
+    {:else}
+       <button type="button" class="variant-ghost-primary btn ml-auto"><Icon name="google" />&nbsp; Google connnected</button>
+  {/if}
 </div>
-
