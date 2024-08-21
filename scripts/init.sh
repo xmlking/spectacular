58f1a922627c02777920c37a23ec810fafdb2d69
Sumanth Chinthagunta <xmlking@gmail.com>
2024-08-21 02:21:07 -0700

    fix: deps updated

--- scripts/init.sh
+++ scripts/init.sh
@@ -32,35 +32,3 @@ if [[ -f "$FILE" || -n "$CI" ]]; then
 else
 	cp -n .secrets.prod.example .secrets.prod && echo 'Generated: .secrets.prod, FIXIT'
 fi
-
-###------Console--------###
-
-FILE=apps/console/.env
-if [[ -f "$FILE" || -n "$CI" ]]; then
-    echo "'$FILE' exists."
-else
-	cp -n apps/console/.env.example apps/console/.env && echo 'Generated: apps/console/.env, FIXIT'
-fi
-
-FILE=apps/console/.secrets
-if [[ -f "$FILE" || -n "$CI" ]]; then
-    echo "'$FILE' exists."
-else
-	cp -n apps/console/.secrets.example apps/console/.secrets && echo 'Generated: apps/console/.secrets, FIXIT'
-fi
-
-###------Console-fb--------###
-
-FILE=apps/console-fb/.env
-if [[ -f "$FILE" || -n "$CI" ]]; then
-    echo "'$FILE' exists."
-else
-	cp -n apps/console-fb/.env.example apps/console-fb/.env && echo 'Generated: apps/console-fb/.env, FIXIT'
-fi
-
-FILE=apps/console-fb/.secrets
-if [[ -f "$FILE" || -n "$CI" ]]; then
-    echo "'$FILE' exists."
-else
-	cp -n apps/console-fb/.secrets.example apps/console-fb/.secrets && echo 'Generated: apps/console-fb/.secrets, FIXIT'
-fi
