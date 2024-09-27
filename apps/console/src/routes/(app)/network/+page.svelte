e690ae4a1397bf806153a1a3fd13e492d9ee9490
Sumanth Chinthagunta <sumanth@chinthagunta.com>
2024-09-26 22:23:55 -0700

    fix(console): add examples for unovis graph

---
+++ apps/console/src/routes/(app)/network/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import NetworkGraph from "./components/network.svelte";
+</script>
+
+<svelte:head>
+  <title>Datablocks | Network</title>
+  <meta name="description" content="Datablocks  Network" />
+</svelte:head>
+
+<div class="page-container">
+  <section class="space-y-4">
+    <h1 class="h1">Network Builder</h1>
+    <p>Virtual Private Network (W.I.P)</p>
+  </section>
+
+  <section class="space-y-4">
+    <h3 class="h3">Builder</h3>
+    <div class="card p-4">
+      <div class="rounded border p-4">
+        <NetworkGraph />
+      </div>
+    </div>
+  </section>
+</div>
---
+++ apps/console/src/routes/(app)/segments/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import LayeredGraphLayout from "./components/layered-graph-layout.svelte";
+</script>
+
+<svelte:head>
+  <title>Datablocks | Microsegmentation</title>
+  <meta name="description" content="Micro Segmentation" />
+</svelte:head>
+
+<div class="page-container">
+  <section class="space-y-4">
+    <h1 class="h1">Micro Segmentation</h1>
+    <p>Network micro segmentation(W.I.P)</p>
+  </section>
+
+  <section class="space-y-4">
+    <h3 class="h3">Microsegmentation</h3>
+    <div class="card p-4">
+      <div class="rounded border p-4">
+        <LayeredGraphLayout />
+      </div>
+    </div>
+  </section>
+</div>
