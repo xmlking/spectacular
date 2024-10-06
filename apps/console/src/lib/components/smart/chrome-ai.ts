17fa35ad22471e68b99a64997763fcafb4a007c0
Sumanth Chinthagunta <xmlking@gmail.com>
2024-10-05 22:59:10 -0700

    fix(console): set ChromeAI only on client side

--- apps/console/src/lib/components/smart/chrome-ai.ts
+++ apps/console/src/lib/components/smart/chrome-ai.ts
@@ -112,9 +112,9 @@ export class ChromeAI {
       }
       this.#isAISupported = true;
 
-      const assistantOptionsSub = assistantOptions.subscribe((options) => {
-        this.#assistantOptions = options;
-      });
+      // const assistantOptionsSub = assistantOptions.subscribe((options) => {
+      //   this.#assistantOptions = options;
+      // });
       // const summarizerOptionsSub = summarizerOptions.subscribe((options) => {
       //   this.#summarizerOptions = options;
       // });
@@ -127,7 +127,7 @@ export class ChromeAI {
 
       onDestroy(async () => {
         this.#log.debug('onDestroy called');
-        assistantOptionsSub();
+        // assistantOptionsSub();
         // summarizerOptionsSub();
         // writerOptionsSub();
         // rewriterOptionsSub();
@@ -137,6 +137,176 @@ export class ChromeAI {
 
   get errors() {
     return readonly(this.#errors);
+  }
+
+  // https://github.com/explainers-by-googlers/prompt-api  #assistant: AIAssistant;
+  async createAssistant(
+    options?: AIAssistantCreateOptionsWithSystemPrompt | AIAssistantCreateOptionsWithoutSystemPrompt,
+  ): Promise<AIAssistant | undefined> {
+    if (this.#isAISupported && window.ai.assistant) {
+      const availability = (await window.ai.assistant?.capabilities())?.available;
+      switch (availability) {
+        case 'readily':
+          return await window.ai.assistant.create({ ...this.#assistantOptions, ...options });
+        case 'after-download':
+          this.#log.error('Built-in assistant model is downloading');
+          this.#errors.update((errors) => {
+            errors.push('Built-in assistant model is downloading');
+            return errors;
+          });
+          return;
+        case 'no':
+          this.#log.error('Built-in assistant model not available');
+          this.#errors.update((errors) => {
+            errors.push('Built-in assistant model not available');
+            return errors;
+          });
+          return;
+        default:
+          this.#log.error('Built-in assistant model not available');
+          this.#errors.update((errors) => {
+            errors.push('Built-in assistant model not available');
+            return errors;
+          });
+          return;
+      }
+    }
+  }
+
+  async createSummarizer(options?: AISummarizerCreateOptions): Promise<AISummarizer | undefined> {
+    if (this.#isAISupported && window.ai.summarizer) {
+      const availability = (await window.ai.summarizer?.capabilities())?.available;
+      switch (availability) {
+        case 'readily':
+          return await window.ai.summarizer.create({ ...this.#summarizerOptions, ...options });
+        case 'after-download':
+          this.#log.error('Built-in summarizer model is downloading');
+          this.#errors.update((errors) => {
+            errors.push('Built-in summarizer model is downloading');
+            return errors;
+          });
+          return;
+        case 'no':
+          this.#log.error('Built-in summarizer model not available');
+          this.#errors.update((errors) => {
+            errors.push('Built-in summarizer model not available');
+            return errors;
+          });
+          return;
+        default:
+          this.#log.error('Built-in summarizer model not available');
+          this.#errors.update((errors) => {
+            errors.push('Built-in summarizer model not available');
+            return errors;
+          });
+          return;
+      }
+    }
+  }
+
+  // https://github.com/tomayac/writer-rewriter-api-playground/tree/main
+  async createWriter(options?: AIWriterCreateOptions): Promise<AIWriter | undefined> {
+    if (this.#isAISupported && window.ai.writer) {
+      return await window.ai.writer.create({ ...this.#writerOptions, ...options });
+      // biome-ignore lint/style/noUselessElse: <explanation>
+    } else {
+      this.#log.error('Built-in writer model not available');
+      this.#errors.update((errors) => {
+        errors.push('Built-in writer model not available');
+        return errors;
+      });
+    }
+  }
+
+  async createRewriter(options?: AIRewriterCreateOptions): Promise<AIRewriter | undefined> {
+    if (this.#isAISupported && window.ai.rewriter) {
+      return await window.ai.rewriter.create({ ...this.#rewriterOptions, ...options });
+      // biome-ignore lint/style/noUselessElse: <explanation>
+    } else {
+      this.#log.error('Built-in rewriter model not available');
+      this.#errors.update((errors) => {
+        errors.push('Built-in rewriter model not available');
+        return errors;
+      });
+    }
+  }
+
+  // https://github.com/tomayac/translation-language-detection-api-playground/blob/main/script.js
+  async createDetector() {
+    if (this.#detector) return this.#detector;
+    if (this.#isAISupported && window.translation) {
+      const availability = await window.translation?.canDetect();
+      switch (availability) {
+        case 'readily':
+          this.#detector = await window.translation.createDetector();
+          return this.#detector;
+        case 'after-download':
+          this.#log.error('Built-in languageDetector model is downloading');
+          this.#errors.update((errors) => {
+            errors.push('Built-in languageDetector model is downloading');
+            return errors;
+          });
+          this.#detector = await window.translation.createDetector();
+          this.#detector.addEventListener('downloadprogress', (e) => {
+            this.#log.info('downloadprogress', e.loaded, e.total);
+          });
+          await this.#detector.ready;
+          return this.#detector;
+        case 'no':
+          this.#log.error('Built-in languageDetector model not available');
+          this.#errors.update((errors) => {
+            errors.push('Built-in languageDetector model not available');
+            return errors;
+          });
+          return;
+        default:
+          this.#log.error('Built-in languageDetector model not available');
+          this.#errors.update((errors) => {
+            errors.push('Built-in languageDetector model not available');
+            return errors;
+          });
+          return;
+      }
+    }
+  }
+
+  // https: //github.com/GoogleChromeLabs/web-ai-demos/blob/main/product-reviews/src/lib/HybridTranslator.js
+  // https://docs.google.com/document/d/1bzpeKk4k26KfjtR-_d9OuXLMpJdRMiLZAOVNMuFIejk/edit
+  async createTranslator(options: TranslationLanguageOptions) {
+    if (this.#isAISupported && window.translation) {
+      const availability = await window.translation.canTranslate(options);
+      switch (availability) {
+        case 'readily':
+          return await window.translation.createTranslator(options);
+        case 'after-download': {
+          this.#log.error('Built-in languageTranslator model is downloading');
+          this.#errors.update((errors) => {
+            errors.push('Built-in languageTranslator model is downloading');
+            return errors;
+          });
+          const translator = await window.translation.createTranslator(options);
+          translator.addEventListener('downloadprogress', (e) => {
+            this.#log.info('downloadprogress', e.loaded, e.total);
+          });
+          await translator.ready;
+          return translator;
+        }
+        case 'no':
+          this.#log.error('Built-in languageTranslator model not available');
+          this.#errors.update((errors) => {
+            errors.push('Built-in languageTranslator model not available');
+            return errors;
+          });
+          return;
+        default:
+          this.#log.error('Built-in languageTranslator model not available');
+          this.#errors.update((errors) => {
+            errors.push('Built-in languageTranslator model not available');
+            return errors;
+          });
+          return;
+      }
+    }
   }
 }
 
