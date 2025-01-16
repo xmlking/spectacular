6583c1cd2be94f5d583b4c43e46ec00d25f06c87
Sumanth Chinthagunta <xmlking@gmail.com>
2025-01-15 20:48:11 -0800

    fix(nhost): adding invitations table

---
+++ apps/console/src/routes/(app)/billing/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import * as Smart from '@spectacular/smart';
+</script>
+
+<svelte:head>
+  <title>Datablocks | billing</title>
+  <meta name="description" content="Org invoices" />
+</svelte:head>
+
+<div class="page-container">
+  <section class="space-y-4">
+    <h1 class="h1">Billing</h1>
+     <p class="text-gray-500 mt-1">Here you can update billing details</p>
+  </section>
+
+  <div class="page-section">
+    <h3 class="h2">Org billing</h3>
+
+  </div>
+</div>
+
