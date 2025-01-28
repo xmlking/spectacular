b7c3d1eaedea1e5a8d779ef474f426ea0f3471d7
Sumanth Chinthagunta <xmlking@gmail.com>
2025-01-28 10:03:41 -0800

    fix(console): adding sys-admin org, role etc

--- nhost/seeds/default/002_user_roles.sql
+++ nhost/seeds/default/002_user_roles.sql
@@ -1,6 +1,7 @@
 SET check_function_bodies = false;
 INSERT INTO auth.user_roles (id, created_at, user_id, role)
 VALUES
+  ('588bbf6a-fb14-41f9-a458-e5dc9e6e3b29', '2024-12-30T06:29:11.368502+00', '8e91bfc4-520d-4349-9888-b628a848f649' , 'sys:admin'),
   ('588bbf6a-fb14-41f9-a458-e5dc9e6e3b29', '2024-12-30T06:29:11.368502+00', '8e91bfc4-520d-4349-9888-b628a848f649' , 'user'),
   ('02fb9614-c959-42af-9e65-cef7cb3a02e6', '2024-12-30T06:29:11.368502+00', '8e91bfc4-520d-4349-9888-b628a848f649' , 'me'),
   ('6cfccf75-d11d-49f7-a28f-f2d857b04638', '2024-12-02 03:17:53.317046+00', '572ad1c0-f97b-4e16-b1f6-8b5ca90f931f', 'user'),
--- nhost/seeds/default/004_memberships.sql
+++ nhost/seeds/default/004_memberships.sql
@@ -1,8 +1,8 @@
 SET check_function_bodies = false;
 INSERT INTO public.memberships ( user_id, org_id, role, created_by, updated_by, created_at, updated_at)
 VALUES
-  ( '572ad1c0-f97b-4e16-b1f6-8b5ca90f931f', '8dfd9a31-de01-47be-92a7-ba1c720c6270', 'org:owner', '572ad1c0-f97b-4e16-b1f6-8b5ca90f931f','572ad1c0-f97b-4e16-b1f6-8b5ca90f931f', '2024-12-02 03:28:40.616639+00', '2024-12-02 03:28:40.616639+00'),
-  ( '572ad1c0-f97b-4e16-b1f6-8b5ca90f931f', 'd5dbb6b6-5e43-4dca-b855-be9b65b6695b', 'org:member', 'bacd19f4-0cc4-43d1-9e7a-4e5098ed8d83','bacd19f4-0cc4-43d1-9e7a-4e5098ed8d83', '2024-12-02 03:28:40.616639+00', '2024-12-02 03:28:40.616639+00'),
-  ( 'bacd19f4-0cc4-43d1-9e7a-4e5098ed8d83', 'd5dbb6b6-5e43-4dca-b855-be9b65b6695b', 'org:owner', 'bacd19f4-0cc4-43d1-9e7a-4e5098ed8d83','bacd19f4-0cc4-43d1-9e7a-4e5098ed8d83', '2024-12-02 03:28:40.616639+00', '2024-12-02 03:28:40.616639+00')
+  ( '572ad1c0-f97b-4e16-b1f6-8b5ca90f931f', 'd5dbb6b6-5e43-4dca-b855-be9b65b6695b', 'org:member', 'bacd19f4-0cc4-43d1-9e7a-4e5098ed8d83','bacd19f4-0cc4-43d1-9e7a-4e5098ed8d83', '2024-12-02 03:28:40.616639+00', '2024-12-02 03:28:40.616639+00')
 
 ON CONFLICT (user_id, org_id) DO NOTHING;
+
+UPDATE public.memberships SET role = 'sys:admin' WHERE user_id = '8e91bfc4-520d-4349-9888-b628a848f649' AND org_id = 'ce1f436e-6e84-4c31-8c78-44d869b27b30';
